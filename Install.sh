4827c075f7f6458cf1d8c4c280068c26a1c62a94
Samuel Vargas <samuel.vg@live.com>
2023-04-20 11:48:19 -0500

    Added Recordings

---
+++ Install.sh
@@ -0,0 +1,38 @@
+cd /usr/src
+
+sudo git clone https://github.com/savagz/voxpbx-asterisk-conf.git
+
+cd /usr/src/voxpbx-asterisk-conf/conf
+
+sudo mv extensions_endpoints.conf /etc/asterisk/.
+sudo mv extensions_features.conf /etc/asterisk/.
+sudo mv extensions_outgoing.conf /etc/asterisk/.
+sudo mv extensions_incoming.conf /etc/asterisk/.
+sudo mv extensions_general.conf /etc/asterisk/.
+sudo mv extensions.conf /etc/asterisk/.
+
+sudo mv pjsip_endpoints.conf /etc/asterisk/.
+sudo mv pjsip_trunks.conf /etc/asterisk/.
+sudo mv pjsip_auth.conf /etc/asterisk/.
+sudo mv pjsip_aors.conf /etc/asterisk/.
+sudo mv pjsip_transport.conf /etc/asterisk/.
+sudo mv pjsip_register.conf /etc/asterisk/.
+sudo mv pjsip.conf /etc/asterisk/.
+
+sudo mv confbridge.conf /etc/asterisk/.
+sudo mv musiconhold.conf /etc/asterisk/.
+sudo mv voicemail.conf /etc/asterisk/.
+
+sudo rm -rf extensions_minivm.conf
+
+cd /etc/asterisk/
+sudo chown asterisk:asterisk *.conf
+
+sudo mkdir /var/lib/asterisk/moh/custom
+sudo mkdir /var/lib/asterisk/moh/devices
+cd /var/lib/asterisk/moh/devices
+sudo mv /usr/src/voxpbx-asterisk-conf/files/moh/Epic_Soul_Factory-Tokyo_Lights.gsm .
+sudo mv /usr/src/voxpbx-asterisk-conf/files/moh/Manu95-A_Long_Way.gsm .
+
+cd /var/lib/asterisk/moh
+sudo chown -R asterisk:asterisk *
