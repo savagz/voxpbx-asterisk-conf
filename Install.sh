03cb2c3aba2b67a26193dfd9ae94ec0d1b18db26
Samuel Vargas <samuel.vg@live.com>
2023-04-25 15:05:45 -0500

    Fixed Path

--- Install.sh
+++ Install.sh
@@ -1,42 +1,45 @@
 #cd /usr/src
-#sudo git clone https://github.com/savagz/voxpbx-asterisk-conf.git /usr/src/conf
+#sudo git clone https://github.com/savagz/voxpbx-asterisk-conf.git /usr/src/voxpbx
 
-sudo mv extensions_endpoints.conf /etc/asterisk/.
-sudo mv extensions_features.conf /etc/asterisk/.
-sudo mv extensions_outgoing.conf /etc/asterisk/.
-sudo mv extensions_incoming.conf /etc/asterisk/.
-sudo mv extensions_general.conf /etc/asterisk/.
-sudo mv extensions.conf /etc/asterisk/.
+# Copy Conf
+sudo mv conf/extensions_endpoints.conf /etc/asterisk/.
+sudo mv conf/extensions_features.conf /etc/asterisk/.
+sudo mv conf/extensions_outgoing.conf /etc/asterisk/.
+sudo mv conf/extensions_incoming.conf /etc/asterisk/.
+sudo mv conf/extensions_general.conf /etc/asterisk/.
+sudo mv conf/extensions.conf /etc/asterisk/.
 
-sudo mv pjsip_endpoints.conf /etc/asterisk/.
-sudo mv pjsip_trunks.conf /etc/asterisk/.
-sudo mv pjsip_auth.conf /etc/asterisk/.
-sudo mv pjsip_aors.conf /etc/asterisk/.
-sudo mv pjsip_transport.conf /etc/asterisk/.
-sudo mv pjsip_register.conf /etc/asterisk/.
-sudo mv pjsip.conf /etc/asterisk/.
+sudo mv conf/pjsip_endpoints.conf /etc/asterisk/.
+sudo mv conf/pjsip_trunks.conf /etc/asterisk/.
+sudo mv conf/pjsip_auth.conf /etc/asterisk/.
+sudo mv conf/pjsip_aors.conf /etc/asterisk/.
+sudo mv conf/pjsip_transport.conf /etc/asterisk/.
+sudo mv conf/pjsip_register.conf /etc/asterisk/.
+sudo mv conf/pjsip.conf /etc/asterisk/.
 
-sudo mv confbridge.conf /etc/asterisk/.
-sudo mv musiconhold.conf /etc/asterisk/.
-sudo mv voicemail.conf /etc/asterisk/.
+sudo mv conf/confbridge.conf /etc/asterisk/.
+sudo mv conf/musiconhold.conf /etc/asterisk/.
+sudo mv conf/voicemail.conf /etc/asterisk/.
 
-sudo mv cdr.conf /etc/asterisk/.
-sudo mv res_odbc.conf /etc/asterisk/.
-sudo mv cdr_adaptive_odbc.conf /etc/asterisk/.
+sudo mv conf/cdr.conf /etc/asterisk/.
+sudo mv conf/res_odbc.conf /etc/asterisk/.
+sudo mv conf/cdr_adaptive_odbc.conf /etc/asterisk/.
 
-sudo rm -rf extensions_minivm.conf
-
+# Conf Removed & Permissions
 cd /etc/asterisk/
+sudo rm -rf extensions_minivm.conf
 sudo chown asterisk:asterisk *.conf
 
+# MoH
 sudo mkdir /var/lib/asterisk/moh/custom
 sudo mkdir /var/lib/asterisk/moh/devices
 cd /var/lib/asterisk/moh/devices
-sudo mv /usr/src/voxpbx-asterisk-conf/files/moh/Epic_Soul_Factory-Tokyo_Lights.gsm .
-sudo mv /usr/src/voxpbx-asterisk-conf/files/moh/Manu95-A_Long_Way.gsm .
+sudo mv /usr/src/voxpbx/files/moh/Epic_Soul_Factory-Tokyo_Lights.gsm .
+sudo mv /usr/src/voxpbx/files/moh/Manu95-A_Long_Way.gsm .
 
 cd /var/lib/asterisk/moh
 sudo chown -R asterisk:asterisk *
 
-cd /usr/src/voxpbx-asterisk-conf/files/odbc
+# ODBC
+cd /usr/src/voxpbx/files/odbc
 sudo mv odbc.ini /etc/.
